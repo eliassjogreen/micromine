2785d745965f45fa7eed37a28acfd05d12dae098
eliassjogreen <eliassjogreen1@gmail.com>
2020-10-04 16:22:09 +0200

    feat: pre-generate prime list

--- miner.ts
+++ miner.ts
@@ -5,7 +5,7 @@ import {
   TaskStoreResultMessage,
 } from "./microgrid.ts";
 import { parry } from "./deps.ts";
-import { check } from "./twin_prime.ts";
+import { check, next, prime, sieve } from "./twin_prime.ts";
 
 export async function work(
   thread: number,
@@ -17,7 +17,7 @@ export async function work(
   console.log(
     `[${
       thread.toString().padStart(2, "0")
-    }][${uid}] Got new Task! Checking sequence: ${start}..${stop}`,
+    }][${uid}] Checking sequence: ${start}..${stop}`,
   );
   const startTime = performance.now();
   const result = await check(start, stop);
@@ -35,24 +35,39 @@ export async function work(
 
 export class Miner {
   public readonly threads;
+  public readonly sieve;
 
   #microgrid: Microgrid;
 
-  constructor(microgrid: Microgrid, threads = 16) {
+  constructor(microgrid: Microgrid, threads = 16, sieve = 1e4) {
     this.threads = threads;
+    this.sieve = sieve;
     this.#microgrid = microgrid;
   }
 
   public async mine() {
+    console.log(`[--] Generating first ${this.sieve} primes for sieve...`);
+    const primes: number[] = [];
+    for (let i = 3; primes.length < this.sieve; i += 2) {
+      if (prime(i)) {
+        primes.push(i);
+      }
+    }
+    
     const workers = new Array(this.threads).fill(undefined).map((_) => {
       const worker = parry(work);
       worker.use("check", check);
+      worker.use("next", next);
+      worker.use("sieve", sieve);
+      worker.use("prime", prime);
+      worker.declare("primes", primes);
       return worker;
     });
     const promises: Array<Promise<[number, number, number, number[]]>> = [];
     console.log(`[--] Starting mining using ${this.threads} workers`);
 
     for (let thread = 0; thread < this.threads; thread++) {
+      console.log(`[--] Fetching new task for thread ${thread}...`);
       const { uid, start_number, stop_number, workunit_result_uid } = await this
         .fetchTask();
       promises[thread] = workers[thread](
@@ -66,8 +81,10 @@ export class Miner {
 
     while (true) {
       const [thread, ...old] = await Promise.race(promises);
+      console.log(`[--] Storing task ${old[1]} for thread ${thread}...`);
       await this.storeTask(old[0], old[1], old[2]);
 
+      console.log(`[--] Fetching new task for thread ${thread}...`);
       const { uid, start_number, stop_number, workunit_result_uid } = await this
         .fetchTask();
       promises[thread] = workers[thread](
@@ -103,7 +120,9 @@ export class Miner {
 
     if (storeResult !== TaskStoreResultMessage.TaskStoreResultSuccessful) {
       console.log(
-        `[--][${uid}] Could not store result, exiting...`,
+        `[--][${uid}] Could not store result (${
+          TaskStoreResultMessage[storeResult]
+        }), exiting...`,
       );
       Deno.exit(1);
     }
---
+++ twin_prime_parallel.ts
@@ -0,0 +1,30 @@
+import { parry } from "./deps.ts";
+import { check } from "./twin_prime.ts";
+
+export async function checkParallel(
+  start: number,
+  stop: number,
+  threads: number,
+): Promise<number[]> {
+  const totalRange = stop - start;
+  const threadRange = Math.floor(totalRange / threads);
+  const promises: Promise<number[]>[] = [];
+  const workers = new Array(threads).fill(undefined).map((_) => parry(check));
+
+  for (let i = 0; i < threads; i++) {
+    promises.push(
+      workers[i](
+        start + threadRange * i,
+        start + threadRange * (i + 1),
+      ),
+    );
+  }
+
+  const result = (await Promise.all(promises)).flat();
+
+  for (const worker of workers) {
+    worker.close();
+  }
+
+  return result;
+}
